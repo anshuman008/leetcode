7d115a0fa176659999b6ae50e1cf8fed914e3923
Anshuman <107368860+anshuman008@users.noreply.github.com>
2023-05-21 16:24:46 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.cpp
+++ Second Largest - GFG/second-largest.cpp
@@ -9,35 +9,27 @@ class Solution{
 public:	
 	// Function returns the second
 	// largest elements
-	int print2largest(int arr[], int n) {
-	    
-	    unordered_set<int> st;
-	
-	    
-	    for(int i = 0; i<n ; i++){
-	        st.insert(arr[i]);
-	    }
-	    
-	    if(st.size() < 2){
-	        return -1;
-	    }
-int max1 = INT_MIN;
-int max2 = INT_MIN;
+	    int print2largest(int arr[], int n) {
+        int max1 = INT_MIN;     // Largest element
+        int max2 = INT_MIN;     // Second largest element
 
-	    for(auto j : st){
-	    if(j > max1){
-        max2 = max1;
-        max1 = j;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max1) {
+                max2 = max1;
+                max1 = arr[i];
+            } else if (arr[i] > max2 && arr[i] < max1) {
+                max2 = arr[i];
+            }
+        }
+
+        if (max2 == INT_MIN) {
+            return -1;  // No second largest element found
+        } else {
+            return max2;
+        }
     }
-    else if(j > max2){
-        max2 = j;
-    }
-	    }
-
-
- return max2;
-	}
 };
+
 
 //{ Driver Code Starts.
 
